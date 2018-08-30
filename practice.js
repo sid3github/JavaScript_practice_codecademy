1837ce6f62f23026fc2d5950395bc10de82c1d5e
Siddharth <siddharthpadwal3@gmail.com>
2018-08-30 14:25:29 +0530

    js functions

--- practice.js
+++ practice.js
@@ -113,9 +113,15 @@ takeOrder('pepperoni', 'brooklyn style');
 const getSubTotal = (itemCount) => {
   return itemCount * 7.5;
 };
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06;
+};
+const getTotal = (itemCount) => {
+  return getSubTotal(itemCount) + getTax(itemCount);
+};
 
 console.log(getSubTotal(orderCount));
-
+console.log(getTotal(orderCount));
 
 
 
